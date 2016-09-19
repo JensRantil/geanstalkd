5f27c62c9c59030b5c6420326b1651f6a39f13bf
Jens Rantil <jens.rantil@gmail.com>
2016-09-19 21:52:40 +0200

    parallelize tests (#41)

--- net_test.go
+++ net_test.go
@@ -70,13 +70,19 @@ func (iot inputOutputTest) ExpectingOutput(t *T, expected string) {
 }
 
 func TestPut(t *T) {
+	t.Parallel()
+
 	testInput("put 0 0 10 5\r\nhello\r\n").ExpectingOutput(t, "INSERTED 1\r\n")
 }
 
 func TestUnknownCommand(t *T) {
+	t.Parallel()
+
 	testInput("this is a test\r\n").ExpectingOutput(t, "UNKNOWN_COMMAND\r\n")
 }
 
 func TestQuitCommand(t *T) {
+	t.Parallel()
+
 	testInput("quit\r\n").ExpectingOutput(t, "")
 }
