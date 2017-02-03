4e5a9144d5b8b7b70553d88f84523db9bcc5bfb0
Jens Rantil <jens.rantil@tink.se>
2017-02-03 22:34:33 +0100

    doc(behaviour): lots of documentation

--- behaviour.go
+++ behaviour.go
@@ -6,6 +6,8 @@ type JobRegistry interface {
 	// identifier already exist, `ErrJobAlreadyExist` returned.
 	Insert(Job) error
 
+	// Update the registry to reflect possible changes made to Job. Returns
+	// `ErrJobMissing` if the Job could not be found.
 	Update(Job) error
 
 	// GetByID returns the job with a given job identifier. If the job can't be
@@ -22,10 +24,31 @@ type JobRegistry interface {
 	GetLargestID() (JobID, error)
 }
 
+// A JobPriorityQueue is a queue which orders jobs according to a specific
+// priority. The queue MAY be backed by a heap, but could equally be backed by
+// a B-tree/LSM on disk.
 type JobPriorityQueue interface {
+	// Notify the priority queue that the Job's priority might have changed and
+	// that internal datastructures must be updated to reflect that. Returns
+	// `ErrJobMissing` if the job was not in the queue.
 	Update(Job) error
+
+	// Remove and return the Job with the highest priority. Returns nil if
+	// there is nothing to return.
+	//
+	// TODO: Check if Go best-practise is to return `nil` or `error` here.
 	Pop() *Job
+
+	// Return the highest priority Job. Return nil if there are no jobs in the
+	// queue.
+	//
+	// TODO: Check if Go best-practise is to return `nil` or `error` here.
 	Peek() *Job
+
+	// Put a new Job on the queue.
 	Push(Job)
+
+	// Remove a Job from the queue with a specific ID.  Returns `ErrJobMissing`
+	// if the job was not in the queue.
 	Remove(JobID) error
 }
