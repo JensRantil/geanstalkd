f6714e80faad898199769110002173a721e25c26
Jens Rantil <jens.rantil@tink.se>
2017-05-27 13:28:58 +0200

    tst(golint): migrate from TravisCI to test
    I noticed that I had forgotten to make sure linting passed. Migrating it
    to tests to be able to uphold some kind of code quality :-)

--- quality_test.go
+++ quality_test.go
@@ -1,7 +1,11 @@
 package geanstalkd
 
 import (
+	"bufio"
+	"bytes"
 	"os/exec"
+	"regexp"
+	"strings"
 	. "testing"
 )
 
@@ -10,5 +14,30 @@ func TestGoVet(t *T) {
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Error("Something is broken. See other errors:\n" + string(output))
+	}
+}
+
+var (
+	golintWarning = regexp.MustCompile("^(?P<file>.*):(?P<line>\\d+):(?P<col>\\d+): (?P<desc>.*)$")
+
+	// AFAIK golint doesn't support a way to ignore warnings. Doing it this way instead.
+	golintIgnores = regexp.MustCompile(strings.Join([]string{
+		"^testing/.*\\.go:\\d+:2: should not use dot imports$",
+	}, "|"))
+)
+
+func TestGoLint(t *T) {
+	cmd := exec.Command("golint", "-set_exit_status", "./...")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		buf := bytes.NewBuffer(output)
+		scanner := bufio.NewScanner(buf)
+		for scanner.Scan() {
+			if match := golintWarning.FindString(scanner.Text()); match != "" {
+				if ignoreCheck := golintIgnores.FindString(scanner.Text()); ignoreCheck == "" {
+					t.Error(scanner.Text())
+				}
+			}
+		}
 	}
 }
