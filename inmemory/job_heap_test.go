bfee44ab35f0223a6b89efe6ec7d462786de4d66
Jens Rantil <jens.rantil@tink.se>
2017-05-28 22:20:06 +0200

    tst(job_heap): remove unnecessary test
    The behaviour tests are covering that test case.

--- inmemory/job_heap_test.go
+++ inmemory/job_heap_test.go
@@ -1,12 +1,10 @@
 package inmemory
 
 import (
-	"container/heap"
 	. "testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 
-	"github.com/JensRantil/geanstalkd"
 	"github.com/JensRantil/geanstalkd/testing"
 )
 
@@ -17,21 +15,4 @@ func TestJobHeapPriorityQueue(t *T) {
 		jpq := NewJobHeapPriorityQueue()
 		testing.GenericJobPriorityQueueTest(jpq)
 	})
-}
-
-func TestInternalHeap(t *T) {
-	t.Parallel()
-
-	h := &jobHeapInterface{
-		jobs: []*geanstalkd.Job{
-			{ID: 40},
-			{ID: 39},
-		},
-		indexByJobID: make(map[geanstalkd.JobID]int),
-	}
-	heap.Init(h)
-
-	if heap.Pop(h).(*geanstalkd.Job).ID != 39 {
-		t.Fail()
-	}
 }
